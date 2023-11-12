4348745f34ab24dc7a6a74af5598441ce92f01b7
gcarnab <gcarnab74@gmail.com>
2023-11-12 08:53:28 +0100

    home

--- gc_java_template_mvn/src/main/java/com/template/gc/Myclass.java
+++ gc_java_template_mvn/src/main/java/com/template/gc/Myclass.java
@@ -1,13 +1,15 @@
 package com.template.gc;
 
 /**
- * The class "Myclass" is a Java class that has a private instance variable "name" and getter and
+ * The class "Myclass" is a Java class that has a private instance variable
+ * "name" and getter and
  * setter methods for accessing and modifying the value of "name".
+ * 
  * @author [GCARNAB]
  * @version [1.0]
  */
 public class Myclass {
-        private String name;
+    private String name;
 
     public Myclass(String name) {
         this.name = name;
@@ -19,5 +21,14 @@ public class Myclass {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    /**
+     * The function returns a greeting message.
+     * 
+     * @return The string "Hello GCARNAB" is being returned.
+     */
+    public String Greetings(){
+        return "Hello GCARNAB";
     }
 }
